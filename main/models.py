bfd006f7dd396db785bb634e59953a52fc84ce5a
krast-napoli <44581068+krast-napoli@users.noreply.github.com>
2019-11-24 00:03:47 +0300

    Update models.py - insert correct columns

--- main/models.py
+++ main/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
-#class Trains(models.Model):
-#    vagon_number = models.PositiveIntegerField()
-#    train_index = models.PositiveIntegerField()
-#    train_number = models.PositiveIntegerField()
+class Trains(models.Model):
+    ordNumber = models.IntegerField(primary_key=True)
+    carNumber = models.IntegerField()
+    trainIndex = models.IntegerField()
+    trainNumber = models.IntegerField()
+    carStatus = models.CharField(max_length=40)
+    invoiceId = models.IntegerField()
+    invoiceNumber = models.CharField(max_length=8)
+    stateId = models.IntegerField()
+    lastOperDt = models.DateTimeField(max_length=10)
