6149dc5ba83268f64bbd12519f7783a05acc97a8
krast-napoli <44581068+krast-napoli@users.noreply.github.com>
2019-11-24 18:15:05 +0300

    Update fields in models.py and update README.MD

--- main/models.py
+++ main/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 class Trains(models.Model):
     ordNumber = models.IntegerField(primary_key=True)
-    carNumber = models.IntegerField()
-    trainIndex = models.IntegerField()
-    trainNumber = models.IntegerField()
+    carNumber = models.IntegerField(null=True, blank=True)
+    trainIndex = models.IntegerField(null=True, blank=True)
+    trainNumber = models.IntegerField(null=True, blank=True)
     carStatus = models.CharField(max_length=40)
-    invoiceId = models.IntegerField()
+    invoiceId = models.IntegerField(null=True, blank=True)
     invoiceNumber = models.CharField(max_length=8)
-    stateId = models.IntegerField()
-    lastOperDt = models.DateTimeField(max_length=10)
+    stateId = models.IntegerField(null=True, blank=True)
+    lastOperDt = models.DateTimeField(max_length=10, null=True, blank=True)
